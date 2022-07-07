464f49c18fb38b3bec315a06bba7655b1a48b2e2
Christian QH ✔ <christian.quispeh@gmail.com>
2022-07-07 17:34:10 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, {isServer}) => {
+      if (isServer) {
+          config.externals.push('_http_common');
+        // config.externals.push('encoding');
+      }
+      return config;
+  },
   target: 'experimental-serverless-trace',
 };
